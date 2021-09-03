316366128d35a6e9b2ee64563ae3ed75cbdcf553
ahmtkzk <ahmetkazak08@gmail.com>
2021-09-04 00:05:22 +0300

    Create JpaCrudSimpleApplication.java

---
+++ src/main/java/com/ahmtkzk/JPAcrudsimple/JpaCrudSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.ahmtkzk.JPAcrudsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaCrudSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaCrudSimpleApplication.class, args);
+	}
+
+}
